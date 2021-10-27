975909c8f5a2764ad09d589da3d242891d9edecc
Alex Lucas <alexlucas_94@hotmail.com>
2021-10-27 11:40:49 -0300

    Exercicio Uri

---
+++ uri 1144/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int n = sc.nextInt();
+		
+		    for(int i=1; i<= n; i++) {
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", i ,segundo,terceiro);
+			System.out.printf("%d %d %d%n", i ,segundo + 1,terceiro + 1);
+
+			
+		}
+		sc.close();
+	}
+
+}
