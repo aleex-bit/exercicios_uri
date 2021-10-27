781f4e31886b812b9402cffa574563455cb0edfd
Alex Lucas <alexlucas_94@hotmail.com>
2021-10-27 10:44:42 -0300

    Exercicio Uri

---
+++ uri1004/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int A, B, PROD;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		PROD = A * B;
+		
+		System.out.println("PROD = " + PROD);
+				
+
+		
+		sc.close();
+	}
+
+}
