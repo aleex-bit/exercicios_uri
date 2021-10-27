3fe114f418495c3666d66e8370d70a20e43dbaa1
Alex Lucas <alexlucas_94@hotmail.com>
2021-10-27 11:45:40 -0300

    Exercicio Uri

---
+++ uri 1158/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+ 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		 for(int i = 0; i<n;i++ ) {
+			 
+			 int x = sc.nextInt();
+			 int y = sc.nextInt();
+			 
+			 int soma = 0;
+			 int cont =0;
+			 
+			 for(int a = x; cont < y; a++) {
+				 
+				 if(a % 2 != 0) {
+					 
+					 soma += a;
+					 cont += 1;
+					 
+				 }
+				 
+			 }
+			 System.out.println(soma);
+		 }
+		sc.close();
+	}
+
+}
