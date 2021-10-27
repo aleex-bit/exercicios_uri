3e44807b6ec9daf36b82f43292fa47bd64ce38c2
Alex Lucas <alexlucas_94@hotmail.com>
2021-10-27 11:03:08 -0300

    Exercicio Uri

---
+++ uri1016/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	int distanciaKm, tempoMin;
+	
+	distanciaKm = sc.nextInt();
+	
+	tempoMin = 2 * distanciaKm;
+	
+	System.out.println(tempoMin + " minutos ");
+	
+	
+	sc.close();
+
+	}
+
+}
